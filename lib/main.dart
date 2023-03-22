8236f699da0e9f4a775c36601ef031c89c121573
KobayashiShintaro <shintaroman221@gmail.com>
2023-03-22 09:48:39 +0900

    別バージョン追加
    zissenブランチとは別バージョンのコードを書いてみた

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:freezed/builder.dart';
-import 'package:flutter_reactive_ble/flutter_reactive_ble.dart';
+import  'package:flutter_reactive_ble/flutter_reactive_ble.dart';
 import 'package:async/async.dart';
 
 void main() {
@@ -19,86 +19,93 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'ble_tushin'),
+      home: DeviceListWidget(title: 'ble_tushin'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+class BluetoothService {
+  late FlutterReactiveBle _ble;
 
-  final String title;
+  BluetoothService() {
+     _ble = FlutterReactiveBle();
+  }
+
+  Stream<List<DiscoveredDevice>> scanForDevices({Duration scanTimeout= const Duration(seconds: 10)}) {
+  return _ble.scanForDevices(
+    withServices: [],
+    scanMode: ScanMode.balanced,
+    requireLocationServicesEnabled: true,
+    // 10秒スキャン
+  ).map((devices) => [devices]);
+}
+
+  Future<List<BluetoothService>> getServices(String deviceId) async {
+    final connectedDevice = await _ble.connectToDevice(id: deviceId).first;
+    return connectedDevice.discoverServices().first;
+  }
+}
+
+class DeviceListWidget extends StatefulWidget {
+   const DeviceListWidget({Key? key, required this.title}) : super(key: key);
+  @override
+  _DeviceListWidgetState createState() => _DeviceListWidgetState();
+}
+
+class _DeviceListWidgetState extends State<DeviceListWidget> {
+  final _bluetoothService = BluetoothService();
+
+  List<DiscoveredDevice> _devices = [];
+  DiscoveredDevice _selectedDevice;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  final _flutterReactiveBle = FlutterReactiveBle();
-
-  StreamSubscription<List<ScanResult>> _scanSubscription;
-  List<ScanResult> _scanResults = [];
-
-  ScanResult _toScanResult(DiscoveredDevice device) {
-    return ScanResult(
-      device: device,
-      advertisementData: AdvertisementData(),
-      rssi: -100,
-    );
+  void initState() {
+    super.initState();
+    _startScan();
   }
 
-  void startScan() {
-    _scanSubscription =
-        _flutterReactiveBle.scanForDevices(withServices: []).listen(
-      (discoveredDevice) {
-        final scanResult = _toScanResult(discoveredDevice);
-        setState(() {
-          _scanResults.add(scanResult);
-        });
-      },
-    );
+  @override
+  void dispose() {
+    _stopScan();
+    super.dispose();
+  }
+
+  void _startScan() {
+    _bluetoothService.scanForDevices().listen((devices) {
+      setState(() {
+        _devices = devices;
+      });
+    });
+  }
+
+  void _stopScan() {
+    _bluetoothService.scanForDevices().first.then((_) {
+      _devices.clear();
+      setState(() {});
+    });
+  }
+
+  Future<void> _onDeviceSelected(DiscoveredDevice device) async {
+    final services = await _bluetoothService.getServices(device.id);
+    setState(() {
+      _selectedDevice = device;
+      // 選択したデバイスのサービスを表示
+      print(services);
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Container(
-        height: 300.0,
-        child: ListView.builder(
-          itemCount: _scanResults.length,
-          itemBuilder: (BuildContext context, int index) {
-            final scanResult = _scanResults[index];
-            return ListTile(
-              title: Text(scanResult.device.name ?? "Unknown"),
-              subtitle: Text(scanResult.device.id.toString()),
-              onTap: () {
-                connectToDevice(scanResult.device.id);
-              },
-            );
-          },
-        ),
-      ),
-    );
-  }
-
-  void connectToDevice(Uuid deviceId) async {
-    _scanSubscription?.cancel();
-
-    final deviceConnection = _flutterReactiveBle.connectToDevice(
-      id: deviceId,
-      connectionTimeout: const Duration(seconds: 10),
-    );
-
-    final deviceServices = deviceConnection
-        .flatMap(
-            (connection) => connection.discoverAllServicesAndCharacteristics())
-        .asBroadcastStream();
-
-    deviceServices.listen((event) {
-      // サービスを読み取る処理
-    });
-  }
-}
+      appBar: AppBar(title: Text('Device List')),
+      body: ListView.builder(
+        itemCount: _devices.length,
+        itemBuilder: (BuildContext context, int index) {
+          final device = _devices[index];
+          return ListTile(
+            title: Text(device.name),
+            subtitle: Text(device.id),
+            onTap: () => _onDeviceSelected(device),
+          );
+        }));
+        }
