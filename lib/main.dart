3766eaf0b57543a918a55aa64bf05c7c0afdc7b0
KobayashiShintaro <shintaroman221@gmail.com>
2023-04-16 11:53:10 +0900

    ble通信だけをするアプリの完成（仮）
    坂上さんに紹介していただいたアプリを作っていき、無事エラーは全部消すことができた。まだスマホに映してはない。

--- lib/main.dart
+++ lib/main.dart
@@ -1,57 +1,86 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 import 'package:flutter_reactive_ble/flutter_reactive_ble.dart';
-import 'package:intl/intl.dart';
+import 'package:flutter_bluetooth_tushin_app/src/ble/ble_device_connector.dart';
+import 'package:flutter_bluetooth_tushin_app/src/ble/ble_device_interactor.dart';
+import 'package:flutter_bluetooth_tushin_app/src/ble/ble_scanner.dart';
+import 'package:flutter_bluetooth_tushin_app/src/ble/ble_status_monitor.dart';
+import 'package:flutter_bluetooth_tushin_app/src/ui/ble_status_screen.dart';
+import 'package:flutter_bluetooth_tushin_app/src/ui/device_list.dart';
+import 'package:provider/provider.dart';
+import 'src/ble/ble_logger.dart';
+
+const _themeColor = Colors.lightGreen;
 
 void main() {
-  return runApp(
-    const MaterialApp(home: HomePage()),
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final _ble = FlutterReactiveBle();
+  final _bleLogger = BleLogger(ble: _ble);
+  final _scanner = BleScanner(ble: _ble, logMessage: _bleLogger.addToLog);
+  final _monitor = BleStatusMonitor(_ble);
+  final _connector = BleDeviceConnector(
+    ble: _ble,
+    logMessage: _bleLogger.addToLog,
+  );
+  final _serviceDiscoverer = BleDeviceInteractor(
+    bleDiscoverServices: _ble.discoverServices,
+    readCharacteristic: _ble.readCharacteristic,
+    writeWithResponse: _ble.writeCharacteristicWithResponse,
+    writeWithOutResponse: _ble.writeCharacteristicWithoutResponse,
+    subscribeToCharacteristic: _ble.subscribeToCharacteristic,
+    logMessage: _bleLogger.addToLog,
+  );
+  runApp(
+    MultiProvider(
+      providers: [
+        Provider.value(value: _scanner),
+        Provider.value(value: _monitor),
+        Provider.value(value: _connector),
+        Provider.value(value: _serviceDiscoverer),
+        Provider.value(value: _bleLogger),
+        StreamProvider<BleScannerState?>(
+          create: (_) => _scanner.state,
+          initialData: const BleScannerState(
+            discoveredDevices: [],
+            scanIsInProgress: false,
+          ),
+        ),
+        StreamProvider<BleStatus?>(
+          create: (_) => _monitor.state,
+          initialData: BleStatus.unknown,
+        ),
+        StreamProvider<ConnectionStateUpdate>(
+          create: (_) => _connector.state,
+          initialData: const ConnectionStateUpdate(
+            deviceId: 'Unknown device',
+            connectionState: DeviceConnectionState.disconnected,
+            failure: null,
+          ),
+        ),
+      ],
+      child: MaterialApp(
+        title: 'Flutter Reactive BLE example',
+        color: _themeColor,
+        theme: ThemeData(primarySwatch: _themeColor),
+        home: const HomeScreen(),
+      ),
+    ),
   );
 }
 
-class HomePage extends StatefulWidget {
-  const HomePage({Key? key}) : super(key: key);
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({
+    Key? key,
+  }) : super(key: key);
 
   @override
-  _HomePageState createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: [
-          Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.grey, // background
-                foregroundColor: Colors.white, // foreground
-              ),
-              onPressed: () {},
-              child: const Icon(Icons.search),
-            ),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.grey, // background
-                foregroundColor: Colors.white, // foreground
-              ),
-              onPressed: () {},
-              child: const Icon(Icons.bluetooth),
-            ),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.grey, // background
-                foregroundColor: Colors.white, // foreground
-              ),
-              onPressed: () {},
-              child: const Icon(Icons.celebration_rounded),
-            ),
-          ])
-        ],
-      ),
-    );
-  }
+  Widget build(BuildContext context) => Consumer<BleStatus?>(
+        builder: (_, status, __) {
+          if (status == BleStatus.ready) {
+            return const DeviceListScreen();
+          } else {
+            return BleStatusScreen(status: status ?? BleStatus.unknown);
+          }
+        },
+      );
 }
